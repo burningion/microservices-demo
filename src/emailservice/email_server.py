926c518d31246ced1d165880bf86dfcb913dd369
Kirk Kaiser <kirk@zothcorp.com>
2019-06-14 16:38:11 -0400

    Instrument 2 python services

--- src/emailservice/email_server.py
+++ src/emailservice/email_server.py
@@ -14,6 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import ddtrace
+ddtrace.patch_all()
+
 from concurrent import futures
 import argparse
 import os
